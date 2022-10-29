ee95f1d0ee6bde5a830a542366b12ac6cb1ef989
LucaBrasi69 <animal2009@mail.ru>
2022-10-29 17:48:27 +0300

    Second commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,7 +8,7 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCalculateRemainAmount1000() {
         int actual = service.remain(1000);
-        int expected = 0;
+        int expected = 1000;
 
         assertEquals(actual, expected);
     }
